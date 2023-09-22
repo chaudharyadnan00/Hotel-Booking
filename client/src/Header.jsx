163d1a75037093be73d9b475018bacc4d8e65332
chaudharyadnan00 <chaudharyadnan00@gmail.com>
2023-09-22 15:22:29 +0530

    packages updated

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 -rotate-90 text-2xl">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                 </svg>
-                <span className="font-bold text-2xl">airbnc</span>
+                <span className="font-bold text-2xl">airbnb</span>
             </Link>
             <div className='flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
                 <div>Anywhere</div>
